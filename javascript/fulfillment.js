7ef19c5e86c62edc72ab6dafb9b7d9511b2a868e
BandoftheHawk <timojrvnn113@gmail.com>
2019-10-29 22:34:21 +1100

    Basic print row

---
+++ javascript/fulfillment.js
@@ -0,0 +1,16 @@
+(function () {
+    // Add Click even listener to print icons
+    let icons = document.querySelectorAll('.print-icon');
+    icons.forEach((icon) => {
+        icon.addEventListener('click', (e) => {
+            e.preventDefault();
+            let rowToPrint = e.target.parentNode.parentNode.parentNode;
+           console.log(rowToPrint);
+            let newWin = window.open("");
+            newWin.document.write(rowToPrint.outerHTML);
+            newWin.print();
+            newWin.close();
+        });
+    });
+
+})();
